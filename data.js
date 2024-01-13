d9fb67aa17e8a27dbcc4257379416a941945d6d8
Logan Lin <ylincs@bjtu.edu.cn>
2024-01-13 15:27:57 +0800

    Adjust arrangement of projects and pubs.

---
+++ data.js
@@ -0,0 +1,182 @@
+const basic = {
+    "title": "Yan Lin's Scholar Page"
+}
+
+const profile = {
+    "bio": [
+        "I received the B.S. degree in computer science from Beijing Jiaotong University, Beijing, China, in 2019.",
+        "I am currently working toward the Ph.D. degree in the School of Computer and Information Technology, Beijing Jiaotong University. My research interests include spatio-temporal data mining and representation learning."
+    ],
+    "footnote": [
+        {
+            "icon": "geo-alt",
+            "text": "Beijing Jiaotong University, Haidian District, Beijing 100044, China"
+        },
+        {
+            "icon": "translate",
+            "text": "Score Reading 26, Listening 29, Speaking 19, Writing 24 in TOFEL iBT'18"
+        }
+    ],
+    "link": [
+        {
+            "icon": "envelope",
+            "text": "Email",
+            "url": "mailto:ylincs@bjtu.edu.cn",
+            "tip": "ylincs@bjtu.edu.cn"
+        },
+        {
+            "icon": "github",
+            "text": "GitHub",
+            "url": "https://github.com/Logan-Lin",
+            "tip": "My GitHub homepage"
+        },
+        {
+            "icon": "google",
+            "text": "Scholar",
+            "url": "https://scholar.google.com/citations?user=nHMmG2UAAAAJ",
+            "tip": "My Google Scholar page"
+        },
+        {
+            "icon": "steam",
+            "text": "Steam",
+            "url": "https://steamcommunity.com/profiles/76561198241353020/",
+            "tip": "My Steam user page"
+        },
+        {
+            "icon": "play-circle",
+            "text": "Bangumi",
+            "url": "https://bgm.tv/user/logan_lin",
+            "tip": "An anime critique website"
+        },
+        {
+            "icon": "play-btn",
+            "text": "BiliBili",
+            "url": "https://space.bilibili.com/2480785",
+            "tip": "My BiliBili home page"
+        }
+    ]
+}
+
+const projects = [
+    {
+        "img": "promptgenius.webp",
+        "text": "Prompt Genius: Comprehensive Prompt Library for AI ChatBots",
+        "url": "https://www.promptgenius.site/",
+        "note": "All-purpose prompts with hierarchical classification system and multi-language support."
+    },
+    {
+        "img": "overleafcopilot.webp",
+        "text": "Overleaf Copilot: A comprehensive scholar writing copilot for Overleaf",
+        "url": "https://overleafcopilot.github.io/",
+        "note": "Powered by Large Language Models, OverleafCopilot is a plugin designed to elevate the academic writing experience on Overleaf."
+    },
+    {
+        "img": "spatial-temporal big data.webp",
+        "text": "Spatial-temporal Traffic Big Data Mining Platform",
+        "url": "http://demo.insis.com.cn:8200/",
+        "note": "Integrated platform for lubricating the process of spatial-temporal traffic big data mining, including data pre-processing, storing and prediction."
+    },
+    {
+        "text": "Dynamic Prediction Algorithm of Airline Catering Backup Quantity",
+        "note": "Predicting the airline catering backup quantity to help airline companies reduce economic and productivity losses."
+    },
+    {
+        "img": "highway simulation.webp",
+        "text": "Highway Toll Rate Optimization System Based on Traffic Big Data",
+        "url": "http://demo.insis.com.cn:8202/",
+        "note": "A traffic flow simulation system based on historical traffic trajectories and optimal cost algorithm."
+    },
+    {
+        "img": "graph cut.webp",
+        "text": "Robust and Balanced Graph Partition Algorithm for Map Services",
+        "note": "Co-developed with Tencent, a preliminary algorithm for efficient hierarchical routing on large-scale road networks."
+    }
+]
+
+const publications = [
+    {
+        "img": "dot.webp",
+        "title": "Origin-Destination Travel Time Oracle for Map-based Services",
+        "url": "https://github.com/Logan-Lin/DOT",
+        "received": "SIGMOD'24",
+        "authors": "<strong>Yan Lin</strong>, Huaiyu Wan, Jilin Hu, Shengnan Guo, Bin Yang, Christian S. Jensen, Youfang Lin"
+    },
+    {
+        "img": "mmtec.webp",
+        "title": "Pre-training General Trajectory Embeddings with Maximum Multi-view Entropy Coding",
+        "url": "https://arxiv.org/abs/2207.14539",
+        "received": "TKDE'23",
+        "authors": "<strong>Yan Lin</strong>, Huaiyu Wan, Shengnan Guo, Jilin Hu, Christian S. Jensen, Youfang Lin"
+    },
+    {
+        "img": "ctle.webp",
+        "title": "Pre-training Context and Time Aware Location Embeddings from Spatial-Temporal Trajectories for User Next Location Prediction",
+        "url": "https://ojs.aaai.org/index.php/AAAI/article/view/16548",
+        "received": "AAAI'21",
+        "authors": "<strong>Yan Lin</strong>, Huaiyu Wan, Shengnan Guo, Youfang Lin"
+    },
+    {
+        "img": "tale.webp",
+        "title": "Pre-training Time-Aware Location Embeddings from Spatial-Temporal Trajectories",
+        "url": "https://ieeexplore.ieee.org/abstract/document/9351627/",
+        "received": "TKDE'21",
+        "authors": "Huaiyu Wan, <strong>Yan Lin</strong>, Shengnan Guo, Youfang Lin"
+    },
+    {
+        "title": "Inductive and Adaptive Graph Convolution Networks Equipped with Constraint Task for Spatial-Temporal Traffic Data Kriging",
+        "url": "https://www.sciencedirect.com/science/article/pii/S0950705123010730",
+        "received": "KBS'23",
+        "authors": "Tonglong Wei, Youfang Lin, Shengnan Guo, <strong>Yan Lin</strong>, Yiji Zhao, Xiyuan Jin, Zhihao Wu, Huaiyu Wan"
+    },
+    {
+        "title": "WITRAN: Water-wave Information Transmission and Recurrent Acceleration Network for Long-range Time Series Forecasting",
+        "url": "https://openreview.net/forum?id=y08bkEtNBK",
+        "received": "NeurIPS'23",
+        "authors": "Yuxin Jia, Youfang Lin, Xinyan Hao, <strong>Yan Lin</strong>, Shengnan Guo, Huaiyu Wan"
+    },
+    {
+        "title": "Contrastive Pre-training with Adversarial Perturbations for Check-In Sequence Representation Learning",
+        "url": "https://ojs.aaai.org/index.php/AAAI/article/view/25546",
+        "received": "AAAI'23",
+        "authors": "Letian Gong, Youfang Lin, Shengnan Guo, <strong>Yan Lin</strong>, Tianyi Wang, Erwen Zheng, Zeyu Zhou, Huaiyu Wan"
+    },
+    {
+        "title": "Adversarial self-attentive time-variant neural networks for multi-step time series forecasting",
+        "url": "https://www.sciencedirect.com/science/article/abs/pii/S0957417423012241",
+        "received": "Expert Systems with Applications",
+        "authors": "Changxia Gao, Ning Zhang, Youru Li, <strong>Yan Lin</strong>, Huaiyu Wan"
+    },
+    {
+        "title": "Multi-scale adaptive attention-based time-variant neural networks for multi-step time series forecasting",
+        "url": "https://www.sciencedirect.com/science/article/abs/pii/S0957417423012241",
+        "received": "Applied Intelligence",
+        "authors": "Changxia Gao, Ning Zhang, Youru Li, <strong>Yan Lin</strong>, Huaiyu Wan"
+    }
+]
+
+const services = [
+    {
+        "text": "CVPR Reviewer",
+        "time": "2024"
+    },
+    {
+        "text": "AAAI Program Committee",
+        "time": "2024"
+    },
+    {
+        "text": "AAAI, IJCAI, KDD, TKDE, WSDM, WWW, KBS, Volunteer Reviewer",
+        "time": "2019, 2020, 2021, 2022, 2023"
+    },
+    {
+        "text": "ICDE, External Reviewer",
+        "time": "2023"
+    },
+    {
+        "text": "Deep Learning, Teaching Assistant, BJTU",
+        "time": "Fall 2021"
+    },
+    {
+        "text": "Machine Learning, Teaching Assistant, BJTU",
+        "time": "Fall 2021"
+    },
+]
---
+++ script.js
@@ -0,0 +1,69 @@
+$('#page-title, #html-title').text(basic['title']);
+
+profile['bio'].forEach((item) => {
+    var p = $('<p class="card-text">');
+    p.text(item);
+    $('#bio').append(p);
+})
+
+profile['footnote'].forEach(({ icon, text }, i) => {
+    var div = $('<div class="card-text text-body-secondary small">');
+    div.append($(`<i class="bi bi-${icon}">`));
+    div.append($(`<span>${text}</span>`));
+    $('#footnote').append(div);
+})
+
+profile['link'].forEach(({ icon, text, url, tip }) => {
+    var a = $(`<a href="${url}" class="btn btn-light mt-2 me-2" target="_blank" data-bs-toggle="tooltip" data-bs-title="${tip}">`);
+    a.append($(`<i class="bi bi-${icon} me-1">`));
+    a.append($(`<span>${text}</span>`));
+    var tip = new bootstrap.Tooltip(a);
+    $('#link').append(a);
+})
+
+projects.forEach(({ img, text, url, note }) => {
+    var col = $('<div class="col">');
+    if (url) {
+        var card = $(`<a class="card" href="${url}" target="_blank" style="text-decoration:none">`);
+    } else {
+        var card = $('<div class="card">')
+    }
+    if (img) {
+        card.append($(`<img src="assets/projects/${img}" width="100%" height="100%" class="card-img-top">`));
+    }
+    card.append($(`<div class="card-body"><div class="card-text">${text} <div class="small text-secondary">${note}</div></div></div>`));
+    col.append(card);
+    $('#project-cards').append(col);
+})
+
+publications.forEach(({ img, title, url, received, authors }) => {
+    var col = $('<div class="col">');
+    if (url) {
+        var card = $(`<a class="card" href="${url}" target="_blank" style="text-decoration:none">`);
+    } else {
+        var card = $('<div class="card">')
+    }
+    if (img) {
+        card.append($(`<img src="assets/publications/${img}" width="100%" height="100%" class="card-img-top">`));
+    }
+    card.append($(`<div class="card-body"><div class="card-text"> 
+        <span class="badge rounded-pill text-bg-success me-1">${received}</span>
+        ${title}
+        <div class="small text-secondary">${authors}</div>
+        </div></div>`));
+    col.append(card);
+    $('#publication-cards').append(col);
+})
+
+services.forEach(({ text, time }) => {
+    $('#service-list').append($(`
+        <a class="list-group-item">
+            <div class="d-flex w-100 justify-content-between">
+                <div>
+                    <p class="my-1">${text}</p>
+                </div>
+                <small class="text-body-secondary my-1">${time}</small>
+            </div>
+        </a>
+    `));
+})
