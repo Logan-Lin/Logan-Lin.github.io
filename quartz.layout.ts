390d3be79cd027032a5d63a595afe9f81bf600ca
Yan Lin <lyan@cs.aau.dk>
2024-09-10 09:16:48 +0200

    Quartz sync: Sep 10, 2024, 9:16 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -47,5 +47,9 @@ export const defaultListPageLayout: PageLayout = {
     Component.Darkmode(),
     Component.DesktopOnly(Component.Explorer()),
   ],
-  right: [],
+  right: [
+    Component.Graph(),
+    Component.DesktopOnly(Component.TableOfContents()),
+    Component.Backlinks(),
+  ],
 }
